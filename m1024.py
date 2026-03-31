2c031c92c7f722c9442c7fc965f39c6629d56359
luvid-lexus <codic2880@naver.com>
2026-03-31 14:15:24 +0900

    1024번 중간 풀이

--- m1024.py
+++ m1024.py
@@ -3,16 +3,21 @@ input=sys.stdin.readline
 
 N,L=input().split()
 N,L=int(N),int(L)
+x=0
 
 while L<=100:
     if N-L*(L-1)*0.5 > 0 and (N-L*(L-1)*0.5)%L == 0:
-        x=int((N-L*(L-1)*0.5)%L)
+        x=int((N-L*(L-1)*0.5)/L)
         break
     else:
         L+=1
 
 result=[]
-for i in range(L):
-    result.append(x+i)
+if x != 0:
+    for i in range(L):
+        result.append(x+i)
 
-print(*result)
+if result == []:
+    print(-1)
+else:
+    print(*result)
