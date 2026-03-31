15b6488aeb40d80597bdb549b1beae30b3cf4a00
luvid-lexus <codic2880@naver.com>
2026-03-31 14:17:23 +0900

    1024번 정답

--- m1024.py
+++ m1024.py
@@ -3,17 +3,17 @@ input=sys.stdin.readline
 
 N,L=input().split()
 N,L=int(N),int(L)
-x=0
+x=-1
 
 while L<=100:
-    if N-L*(L-1)*0.5 > 0 and (N-L*(L-1)*0.5)%L == 0:
+    if N-L*(L-1)*0.5 >= 0 and (N-L*(L-1)*0.5)%L == 0:
         x=int((N-L*(L-1)*0.5)/L)
         break
     else:
         L+=1
 
 result=[]
-if x != 0:
+if x != -1:
     for i in range(L):
         result.append(x+i)
 
