d478d7e17d89b95a7d6091353e2adbfe50bc8d31
luvid-lexus <codic2880@naver.com>
2026-03-31 10:03:11 +0900

    2331번 중간 풀이

--- m2331.py
+++ m2331.py
@@ -3,18 +3,17 @@ input=sys.stdin.readline
 
 A,P=input().split()
 P=int(P)
-D=[]
+D=[A]
 
 while True:
     x=0
     for i in range(len(A)):
         x+=int(A[i])**P
-    print(x)
     if x in D:
         y=D.index(x)
         break
     else:
         D.append(x)
-        A=x
+        A=str(x)
 
 print(y)
