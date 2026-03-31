cb183161c62cdf26e5885b1089caa6d22a46616d
luvid-lexus <codic2880@naver.com>
2026-03-31 09:30:52 +0900

    2012번 풀이 시작

---
+++ m2012.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+l=[]
+
+for _ in range(N):
+    l.append(int(input()))
+
+l=sorted(l)
+result=0
+
+for i in range(len(l)-1):
+    result+=abs(l[i]-i-1)
+
+print(result)
--- m2331.py
+++ m2331.py
@@ -1,3 +1,8 @@
 import sys
 input=sys.stdin.readline
 
+A,P=input().split()
+A,P=int(A),int(P)
+
+while True:
+    
