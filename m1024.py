2c33ca518651386d4fa36790eee40f1084f8381f
luvid-lexus <codic2880@naver.com>
2026-03-31 14:03:39 +0900

    1024번 풀이 시작

--- m1024.py
+++ m1024.py
@@ -2,4 +2,17 @@ import sys
 input=sys.stdin.readline
 
 N,L=input().split()
+N,L=int(N),int(L)
 
+while L<=100:
+    if N-L*(L-1)*0.5 > 0 and (N-L*(L-1)*0.5)%L == 0:
+        x=int((N-L*(L-1)*0.5)%L)
+        break
+    else:
+        L+=1
+
+result=[]
+for i in range(L):
+    result.append(x+i)
+
+print(*result)
