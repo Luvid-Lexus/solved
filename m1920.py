88a3af8279f43974203f601691a4a01700c8c604
luvid-lexus <codic2880@naver.com>
2026-04-14 09:53:18 +0900

    1920번 정답

--- m1920.py
+++ m1920.py
@@ -2,20 +2,24 @@ import sys
 input=sys.stdin.readline
 
 N=int(input())
-A=sorted(set(map(int,input().split())))
+A=sorted(list(map(int, input().split())))
 M=int(input())
-B=set(map(int,input().split()))
+B=list(map(int, input().split()))
 
 for i in B:
-    s=A[0]
-    e=A[-1]
+    s=0
+    e=len(A)-1
+    result=False
     while s<=e:
         m=(s+e)//2
-        if i>=m:
+        if i==A[m]:
+            result=True
+            break
+        elif i>A[m]:
             s=m+1
         else:
             e=m-1
-    if e==i:
+    if result:
         print(1)
     else:
         print(0)
