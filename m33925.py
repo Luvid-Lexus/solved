062bc7845ea3ac5b7035b86151e5efea32cd6f99
luvid-lexus <codic2880@naver.com>
2026-04-14 14:21:07 +0900

    33925번 중간풀이

--- m33925.py
+++ m33925.py
@@ -1,3 +1,26 @@
 import sys
 input=sys.stdin.readline
 
+N,J,S,H,K = map(int, input().split())
+f3 = input().rstrip()
+f2 = input().rstrip()
+f1 = input().rstrip()
+jump=0
+d_jump=0
+slide=0
+
+for i in range(N):
+    if f1[i] == '^':
+        if f2[i] == '^':
+            d_jump+=1
+        else:
+            jump+=1
+    elif f2[i] == 'v':
+        slide+=1
+
+
+
+if H>0:
+    print(H)
+else:
+    print(-1)
